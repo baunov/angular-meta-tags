c23f05025d362cebab0e5be28467d3c50ef77396
Daniil <daniil@baunov.io>
2020-06-14 17:03:37 +0300

    remove unused

--- src/app/tokens/current-route-params.ts
+++
@@ -1,3 +0,0 @@
-import {InjectionToken} from '@angular/core';
-
-export const CURRENT_ROUTE_PARAMS = new InjectionToken<string>('CurrentRouteParams');
--- src/app/tokens/current-route.ts
+++
@@ -1,3 +0,0 @@
-import {InjectionToken} from '@angular/core';
-
-export const CURRENT_ROUTE = new InjectionToken<string>('CurrentRoute');
